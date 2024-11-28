2292abef831527df2ee8ea758b9c09186f4e021c
houda-LQ <houda.loqmane02@gmail.com>
2024-11-28 10:33:13 +0100

    add js

--- Page.js
+++ Page.js
@@ -1,8 +1,16 @@
-<script>
-    
-    let cartCount = 0;
+// Calculer dynamiquement le total de la commande en fonction de la quantité commandée.
+document.querySelector(".quantity").addEventListener("input",function(){
+    const price = 750;
+    const quantity = parseInt(this.value)|| 1;//1 si aucune valeur n'est saisie
+    const total = price * quantity;//calcul du total
+    document.querySelector(".total").textContent = total + "MAD";//mise a jour du texte
+
+});
+
+//validation du formulaire
+// document.querySelector(".order.form").addEventListener("submit",function(e) {
+
+// )}
 
 
 
-
-</script>
